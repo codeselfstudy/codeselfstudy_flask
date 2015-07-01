c8362d15e38ecbb66e6039c1a85f5d5b91f9ae1f
J. Cohen <hostelmarketing@gmail.com>
2015-07-01 16:04:08 -0700

    Add initial db connection

--- app/codeselfstudy/__init__.py
+++ app/codeselfstudy/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 from .views.pages import pages_bp
 
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 
 # Import routes and logic for each section of the website from the views directory
 app.register_blueprint(pages_bp)
 
+# Database
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
+
+db = SQLAlchemy(app)
---
+++ app/codeselfstudy/models/posts.py
@@ -0,0 +1,7 @@
+from . import db
+
+class Engine(db.Model):
+    # Columns
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(128))
+    thrust = db.Column(db.Integer, default=0)
