806437752d25f98841ac568ebae9350d8da9792a
J. Cohen <hostelmarketing@gmail.com>
2015-07-02 12:10:46 -0700

    Add Flask-PageDown for Markdown preview
    This is a feature experiment branch. We can merge things in, or not.

--- app/codeselfstudy/__init__.py
+++ app/codeselfstudy/__init__.py
@@ -3,6 +3,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 from flask_admin import Admin
 from .views.admin import AdminIndexView
 import mongoengine as me
+from flask_pagedown import PageDown
 
 
 # Import routes and logic for each section of the website from the views directory
@@ -21,14 +22,21 @@ app.config.from_pyfile('config.py')
 
 # TODO: move app.debug out of here.
 app.debug = True
+
+# Debug toolbar must come after app.debug = True
 toolbar = DebugToolbarExtension(app)
 
+# Admin dashboard
 admin = Admin(app, name='Admin Panel')
 admin.add_view(AdminIndexView(name='Content', endpoint='someplace'))
 admin.add_view(AdminIndexView(name='Page I', endpoint='page1', category='Content Moderation'))
 admin.add_view(AdminIndexView(name='Page II', endpoint='page2', category='Content Moderation'))
 admin.add_view(AdminIndexView(name='Page III', endpoint='page3', category='Content Moderation'))
 
+# Database
 me.connect(app.config['MONGODB_DB'])
+
+# Markdown
+pagedown = PageDown(app)
 
 register_blueprints(app)
--- app/codeselfstudy/forms.py
+++ app/codeselfstudy/forms.py
@@ -1,10 +1,16 @@
-from flask.ext.wtf import Form
+from flask_wtf import Form
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired
 from .models.post import Post
 
+from flask_pagedown.fields import PageDownField
+from wtforms.fields import SubmitField
+
+class PageDownFormExample(Form):
+    pagedown = PageDownField('Enter your markdown')
+    submit = SubmitField('Submit')
 # Manual example:
 class PostForm(Form):
     title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-
+    content = PageDownField('Content', validators=[DataRequired()])
+    submit = SubmitField('Publish')
