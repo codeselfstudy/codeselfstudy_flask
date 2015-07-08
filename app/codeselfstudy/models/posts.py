36451057eede0ff29ca9e547d8dd131829558b0a
J. Cohen <hostelmarketing@gmail.com>
2015-07-08 16:19:49 -0700

    Fix bug

--- app/codeselfstudy/models/posts.py
+++ app/codeselfstudy/models/posts.py
@@ -7,6 +7,10 @@ import mongoengine as me
 
 # Based on: https://flask-mongoengine.readthedocs.org/en/latest/
 
+class Comment(me.EmbeddedDocument):
+    created_at = me.DateTimeField(default=datetime.datetime.now, required=True)
+    body = me.StringField(verbose_name="Comment", required=True)
+
 class Post(me.Document):
     title = me.StringField(max_length=120, required=True)
     created_at = me.DateTimeField(default=datetime.datetime.now, required=True)
@@ -30,6 +34,3 @@ class Post(me.Document):
     #def live_posts(doc_cls, queryset):
         #return queryset.filter(published=True)
 
-class Comment(me.EmbeddedDocument):
-    created_at = me.DateTimeField(default=datetime.datetime.now, required=True)
-    body = me.StringField(verbose_name="Comment", required=True)
